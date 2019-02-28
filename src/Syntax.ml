f5aeddcad637ecfcfd3a764da29acc5a39548ab4
gentser <aleshin96@rambler.ru>
2019-02-28 16:18:19 +0300

    Remove useless files and add previous Expr.eval implementation

--- src/Syntax.ml
+++ src/Syntax.ml
@@ -1,14 +1,14 @@
 (* Opening a library for generic programming (https://github.com/dboulytchev/GT).
    The library provides "@type ..." syntax extension and plugins like show, etc.
 *)
-open GT 
-    
+open GT
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -22,14 +22,14 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
@@ -37,14 +37,50 @@ module Expr =
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+    let boolToInt b = if b then 1 else 0
+
+    let intToBool i = i != 0
+
+    let fun1 op = fun x1 x2 -> boolToInt(op x1 x2)
+    let fun2 op = fun x1 x2 -> boolToInt (op (intToBool x1) (intToBool x2))
+
+    let evalOperation op =
+       match op with
+
+       (* These opearators were defined in Embedding.ml *)
+       | "+"  -> ( + )
+       | "-"  -> ( - )
+       | "*"  -> ( * )
+       | "/"  -> ( / )
+       | "%"  -> ( mod )
+
+       (* According to 01.pdf result of following operations is converted to int *)
+       | "==" -> fun1 ( == )
+       | "!=" -> fun1 ( != )
+       | "<=" -> fun1 ( <= )
+       | "<"  -> fun1 ( <  )
+       | ">=" -> fun1 ( >= )
+       | ">"  -> fun1 ( >  )
+
+       (* According to 01.pdf arguments of following operations are converted to bool *)
+       | "&&" -> fun2 ( && )
+       | "!!" -> fun2 ( || )
+
+       (* Unknown operator *)
+       | _    -> failwith (Printf.sprintf "Unknown operator");;
+
+    let rec eval state exp =
+       match exp with
+       | Const v -> v
+       | Var x -> state x
+       | Binop (op, x1, x2) -> evalOperation op (eval state x1) (eval state x2)
 
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -57,7 +93,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -66,13 +102,13 @@ module Stmt =
        Takes a configuration and a statement, and returns another configuration
     *)
     let eval _ = failwith "Not implemented yet"
-                                                         
+
   end
 
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
