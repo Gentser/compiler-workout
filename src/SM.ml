a51848272d4b79bf0f9f527ad9c6ef22c70b5e29
gentser <aleshin96@rambler.ru>
2019-03-04 17:22:37 +0300

    Stack machine interpreter is added

--- src/SM.ml
+++ src/SM.ml
@@ -1,15 +1,15 @@
-open GT       
-       
+open GT
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -22,8 +22,28 @@ type config = int list * Syntax.Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+ let process_instruct config instruct =
+    let (stack, c) = config in
+    let (state, input, output) = c in
+      match instruct with
+        | BINOP op -> (match stack with
+                        (* y::x - because order of arguments on the stack is inverted *)
+                        | y::x::rest -> [Syntax.Expr.evalOperation op x y] @ rest, c
+                      )
+        | CONST x  -> [x] @ stack, c
+        | READ     -> (match input with
+                        | x::rest -> [x] @ stack, (state, rest, output)
+                      )
+        | WRITE    -> (match stack with
+                        | x::rest -> rest, (state, input, output @ [x])
+                      )
+        | LD var   -> [state var] @ stack, c
+        | ST var   -> (match stack with
+                        | x::rest -> rest, (Syntax.Expr.update var x state, input, output)
+                      )
+
+   let eval config prog = List.fold_left process_instruct config prog;;
 
 (* Top-level evaluation
 
