138caee9cd7cba624595d7279670551a9f712551
gentser <aleshin96@rambler.ru>
2019-03-06 10:17:05 +0300

    Stack machine is updated from the hw2

--- src/SM.ml
+++ src/SM.ml
@@ -1,16 +1,16 @@
-open GT       
+open GT
 open Language
-       
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -23,8 +23,28 @@ type config = int list * Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+ let process_instruct config instruct =
+    let (stack, c) = config in
+    let (state, input, output) = c in
+      match instruct with
+        | BINOP op -> (match stack with
+                        (* y::x - because order of arguments on the stack is inverted *)
+                        | y::x::rest -> [Syntax.Expr.evalOperation op x y] @ rest, c
+                      )
+        | CONST x  -> [x] @ stack, c
+        | READ     -> (match input with
+                        | x::rest -> [x] @ stack, (state, rest, output)
+                      )
+        | WRITE    -> (match stack with
+                        | x::rest -> rest, (state, input, output @ [x])
+                      )
+        | LD var   -> [state var] @ stack, c
+        | ST var   -> (match stack with
+                        | x::rest -> rest, (Syntax.Expr.update var x state, input, output)
+                      )
+
+   let eval config prog = List.fold_left process_instruct config prog;;
 
 (* Top-level evaluation
 
@@ -41,4 +61,15 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+
+ (* Expression needs in their own compiler *)
+  let rec compileE e = match e with
+      | Syntax.Expr.Const n -> [CONST n]
+      | Syntax.Expr.Var x -> [LD x]
+      | Syntax.Expr.Binop (operator, left, right) -> (compileE left) @ (compileE right) @ [BINOP operator];;
+
+  let rec compile program = match program with
+      | Syntax.Stmt.Assign (x, e) -> (compileE e) @ [ST x]
+      | Syntax.Stmt.Read x -> [READ; ST x]
+      | Syntax.Stmt.Write e -> (compileE e) @ [WRITE]
+      | Syntax.Stmt.Seq (a, b) -> (compile a) @ (compile b);;
