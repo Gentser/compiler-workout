e65018024e551b271635b610d73a532c5b409bb7
gentser <aleshin96@rambler.ru>
2019-03-21 11:57:34 +0300

    Update Language.ml
    - Label 'parse' in Expr parsing is added
    - Operators Skip, If, While, RepeatUntil are added

--- src/Language.ml
+++ src/Language.ml
@@ -92,6 +92,7 @@ module Expr =
     (* Auxiliary function for binary operation performance - instead of function repeat bellow *)
     let parseBinOp op = ostap(- $(op)), (fun x y -> Binop (op, x, y))
     ostap (
+        parse: expr;
         expr:
       	    !(Ostap.Util.expr
       		    (fun x -> x)  (* identity function *)
@@ -125,7 +126,7 @@ module Stmt =
     (* empty statement                  *) | Skip
     (* conditional                      *) | If     of Expr.t * t * t
     (* loop with a pre-condition        *) | While  of Expr.t * t
-    (* loop with a post-condition       *) (* add yourself *)  with show
+    (* loop with a post-condition       *) | RepeatUntil of Expr.t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
     type config = Expr.state * int list * int list
@@ -147,18 +148,47 @@ module Stmt =
         (* <s, i, o> --> <s, i, o @ [ [|e|]s ]> *)
         | Write expression -> (state, input, output @ [Expr.eval state expression])
         (* C1 -S1-> C' -S2-> C2*)
-        | Seq (state1, state2) -> eval (eval config state1) state2;;
+        | Seq (state1, state2) -> eval (eval config state1) state2
+        | Skip -> config
+        | If (e, s1, s2) -> eval config (if Expr.eval state e != 0 then s1 else s2)
+        | While (e, s) ->
+          if Expr.eval state e != 0 then eval (eval config s) statement else config
+        | RepeatUntil (e, s) ->
+          let ((state', _, _) as config') = eval config s in
+          if Expr.eval state' e = 0 then eval config' statement else config';;
 
-    (* Statement parser *)
+    (* Statement parser - more complex structure than previous*)
     ostap (
-      unitStmt:
-  			  "read" "(" x:IDENT ")" {Read x}
-  			| "write" "(" e:!(Expr.expr) ")" {Write e}
-        | x:IDENT ":=" e:!(Expr.expr) {Assign(x, e)};
+      parse  : seq | stmt;
 
-      parse:
-          s:unitStmt ";" rest:parse {Seq(s, rest)}
-        | unitStmt
+      seq    : s1:stmt -";" s2:parse { Seq(s1, s2) };
+      stmt   : read | write | assign | skip |
+               if' | for' | while' | repeat';
+
+      read   : %"read" -"(" x:IDENT -")" { Read x };
+      write  : %"write" -"(" e:!(Expr.parse) -")" { Write e };
+      assign : x:IDENT -":=" e:!(Expr.parse) { Assign (x, e) };
+      skip   : %"skip" { Skip };
+
+      (* Cycles ends with the inverted start-word (ex: if ... fi) *)
+      if'    : %"if" e:!(Expr.parse)
+               %"then" s1:parse
+                 elif' :(%"elif" !(Expr.parse) %"then" parse)*  (* Multiple 'elif' is allowed *)
+                 else' :(%"else" parse)? %"fi"                  (* Contiditional expression *)
+                   {
+                     let else'' = match else' with
+                       | Some t -> t
+                       | None -> Skip
+                     in
+                     let else''' = List.fold_right (fun (e', t') t -> If (e', t', t)) elif' else'' in
+                     If (e, s1, else''')
+                   };
+      for'   : %"for" s1:parse "," e:!(Expr.parse) ","
+               s2:parse %"do" s3:parse %"od" { Seq (s1, While (e, Seq (s3, s2))) };
+      while' : %"while" e:!(Expr.parse)
+               %"do" s:parse %"od" { While (e, s) };
+      repeat': %"repeat" s:parse %"until"
+                e:!(Expr.parse) { RepeatUntil (e, s) }
     )
 
   end
