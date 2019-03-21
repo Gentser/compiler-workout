d6bfc0f1d5b7527be54f999f3a2998a639014434
gentser <aleshin96@rambler.ru>
2019-03-21 12:00:08 +0300

    SM update Stack machine was update for operators Skip, If, While, RepeatUntil (commants LABEL, JMP, CJMP)

--- src/SM.ml
+++ src/SM.ml
@@ -28,28 +28,50 @@ type config = int list * Stmt.config
    Takes an environment, a configuration and a program, and returns a configuration as a result. The
    environment is used to locate a label to jump to (via method env#labeled <label_name>)
 *)
-let process_instruct config instruct =
-   let (stack, c) = config in
-   let (state, input, output) = c in
-     match instruct with
-       | BINOP op -> (match stack with
-                       (* y::x - because order of arguments on the stack is inverted *)
-                       | y::x::rest -> [Language.Expr.evalOperation op x y] @ rest, c
-                     )
-       | CONST x  -> [x] @ stack, c
-       | READ     -> (match input with
-                       | x::rest -> [x] @ stack, (state, rest, output)
-                     )
-       | WRITE    -> (match stack with
-                       | x::rest -> rest, (state, input, output @ [x])
-                     )
-       | LD var   -> [state var] @ stack, c
-       | ST var   -> (match stack with
-                       | x::rest -> rest, (Language.Expr.update var x state, input, output)
-                     )
 
-  let eval config prog = List.fold_left process_instruct config prog;;
-
+(* Hard expression for left-folding - desicion to make rec function *)
+let rec eval env ((stack, ((state, input, output) as c)) as config) = function
+| [] -> config
+| instruction :: rest_instr ->
+     match instruction with
+     | BINOP op ->
+        begin
+          match stack with
+          | y::x::rest -> (* y::x - because order of arguments on the stack is inverted *)
+             eval env ((Language.Expr.evalOperation op x y) :: rest, c) rest_instr
+          | _ -> failwith "SM interpreter error: BINOP"
+        end
+     | CONST v -> eval env (v::stack, c) rest_instr
+     | READ ->
+        begin
+          match input with
+          | x::rest -> eval env (x::stack, (state, rest, output)) rest_instr
+          | _ -> failwith "SM interpreter error: READ"
+        end
+     | WRITE ->
+        begin
+          match stack with
+          | x::rest -> eval env (rest, (state, input, output @ [x])) rest_instr
+          | _ -> failwith "SM interpreter error: WRITE"
+        end
+     | LD x -> eval env ((state x) :: stack, c) rest_instr
+     | ST x ->
+        begin
+          match stack with
+          | z::rest -> eval env (rest, ((Language.Expr.update x z state), input, output)) rest_instr
+          | _ -> failwith "SM interpreter error: ST"
+        end
+     | LABEL l -> eval env config rest_instr
+     | JMP l -> eval env config (env#labeled l)
+     | CJMP (b, l) ->
+        begin
+          match stack with
+          | x::rest ->
+              if (x = 0 && b = "z" || x != 0 && b = "nz")
+              then eval env (rest, c) (env#labeled l)
+              else eval env (rest, c) rest_instr
+          | _ -> failwith "SM interpreter error: stack is empty"
+        end
 
 (* Top-level evaluation
 
@@ -75,13 +97,39 @@ let run p i =
    stack machine
 *)
 let rec compile =
+
   let rec expr = function
-  | Expr.Var   x          -> [LD x]
-  | Expr.Const n          -> [CONST n]
-  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  | Language.Expr.Var   x          -> [LD x]
+  | Language.Expr.Const n          -> [CONST n]
+  | Language.Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
   in
+
+  let label_generator =
+    object
+      val mutable counter = 0
+      method generate =
+        counter <- counter + 1;
+        "l_" ^ string_of_int counter
+    end
+  in
+
   function
-  | Stmt.Seq (s1, s2)  -> compile s1 @ compile s2
-  | Stmt.Read x        -> [READ; ST x]
-  | Stmt.Write e       -> expr e @ [WRITE]
-  | Stmt.Assign (x, e) -> expr e @ [ST x]
+  | Language.Stmt.Seq (s1, s2)   -> compile s1 @ compile s2
+  | Language.Stmt.Read x         -> [READ; ST x]
+  | Language.Stmt.Write e        -> expr e @ [WRITE]
+  | Language.Stmt.Assign (x, e)  -> expr e @ [ST x]
+
+  | Language.Stmt.Skip           -> []
+  | Language.Stmt.If (e, s1, s2) ->
+     let l_else = label_generator#generate in
+     let l_fi = label_generator#generate in
+     (expr e) @ [CJMP ("z", l_else)] @ (compile s1) @ [JMP l_fi] @ [LABEL l_else] @ (compile s2) @ [LABEL l_fi]
+
+  | Language.Stmt.While (e, s) ->
+     let l_expr = label_generator#generate in
+     let l_od = label_generator#generate in
+     [LABEL l_expr] @ (expr e) @ [CJMP ("z", l_od)] @ (compile s) @ [JMP l_expr] @ [LABEL l_od]
+
+  | Language.Stmt.RepeatUntil (e, s) ->
+     let l_repeat = label_generator#generate in
+     [LABEL l_repeat] @ (compile s) @ (expr e) @ [CJMP ("z", l_repeat)]
